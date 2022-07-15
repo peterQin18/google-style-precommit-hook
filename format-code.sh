30fe76bb55fc4fd45fbfc0dcd4a0fb9136fdfb1b
xiaopeng <email>
2022-07-15 17:38:23 +0800

    test pre commit

--- format-code.sh
+++ format-code.sh
@@ -8,6 +8,7 @@ then
 fi
 cd ..
 
+
 changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
 echo $changed_java_files
 java -jar .cache/google-java-format-1.7-all-deps.jar --aosp --replace $changed_java_files
